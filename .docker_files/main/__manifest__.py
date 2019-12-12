d264cfdc5315a18931ebb95c9a00acd1b031a801
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-12-12 10:15:36 -0500

    Initial commit

---
+++ .docker_files/main/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2019 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Main Module',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Install all addons required for testing.',
+    'depends': [
+    ],
+    'installable': True,
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Odoo Timesheet Addons
+
+This repository contains Odoo modules related to the ``Timesheet`` application.
