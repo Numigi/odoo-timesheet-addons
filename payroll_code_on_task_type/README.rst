8ca1e6261d40ba02f11b435f4fdc99170f2d6b8d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-01-21 07:04:24 -0500

    Add module payroll_code_on_task_type

---
+++ payroll_code_on_task_type/README.rst
@@ -0,0 +1,9 @@
+Payroll Code on Task Type
+=========================
+This module adds a field to contain the payroll (earning) code on task types.
+
+.. image:: static/description/task_type.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ payroll_code_on_task_type/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Payroll Earning Code on Task Type',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'LGPL-3',
+    'category': 'Payroll',
+    'summary': 'Adds payroll earning code on task types.',
+    'depends': [
+        'project_task_type',
+    ],
+    'data': [
+        'views/task_type.xml',
+    ],
+    'installable': True,
+}
