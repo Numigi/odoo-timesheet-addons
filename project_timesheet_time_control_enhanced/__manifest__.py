f83bc45bdafb21f61e6bb49aa4d1783a7715784d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-04-08 15:33:02 -0400

    TA#22390 Add module project_timesheet_time_control_enhanced (#19)
    * Add module project_timesheet_time_control_enhanced
    
    * Add missing tests
    
    * Update project_timesheet_time_control_enhanced/models/account_analytic_line.py
    
    Co-Authored-By: Jordi Riera <547282+foutoucour@users.noreply.github.com>
    
    * Update project_timesheet_time_control_enhanced/tests/test_account_analytic_line.py
    
    Co-Authored-By: Jordi Riera <547282+foutoucour@users.noreply.github.com>
    
    Co-authored-by: Jordi Riera <547282+foutoucour@users.noreply.github.com>

---
+++ project_timesheet_time_control_enhanced/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Timesheet Time Control Enhanced",
+    "version": "1.0.0",
+    "category": "Project",
+    "author": "Numigi",
+    "depends": [
+        "project_timesheet_time_control",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ project_timesheet_time_control_enhanced/models/account_analytic_line.py
@@ -0,0 +1,40 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import pytz
+from datetime import datetime, time
+from odoo import _, api, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+
+    _inherit = 'account.analytic.line'
+    _order = 'date_time desc'
+
+    @api.model
+    def create(self, vals):
+        line = super().create(vals)
+
+        if vals.get("date") and not vals.get("date_time"):
+            line.date_time = line._get_datetime_from_date()
+
+        return line
+
+    @api.multi
+    def write(self, vals):
+        super().write(vals)
+
+        if self and vals.get("date") and not vals.get("date_time"):
+            self.write({'date_time': self[0]._get_datetime_from_date()})
+
+        return True
+
+    def _get_datetime_from_date(self):
+        naive_timestamp = datetime.combine(self.date, time.min)
+
+        tz_name = self._context.get('tz') or self.env.user.tz or 'UTC'
+        tz = pytz.timezone(tz_name)
+        tz_timestamp = tz.localize(naive_timestamp)
+
+        utc_datetime = tz_timestamp.astimezone(pytz.utc)
+        return fields.Datetime.to_string(utc_datetime)
