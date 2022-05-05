9c83a6ead430b8c19780c2ba6b2d28582e189ea3
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2022-05-05 09:24:00 -0400

    Add translations

--- timesheet_description_template/__manifest__.py
+++ timesheet_description_template/__manifest__.py
@@ -2,14 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Timesheet View With Employee",
+    "name": "Timesheet Description Template",
     "version": "1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://www.numigi.com",
     "license": "LGPL-3",
     "category": "Timesheet",
-    "summary": "Add employee to the list view of timesheets",
+    "summary": "Autocomplete timesheet descriptions from templates",
     "depends": [
         "hr_timesheet",
     ],
