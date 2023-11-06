75561a14dc3a559152cac1412395e20ba56eb7b9
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-11-06 15:24:54 +0300

    debug UNIT TEST

--- timesheet_holidays_time_control_compatible/tests/test_validate_holiday_date.py
+++ timesheet_holidays_time_control_compatible/tests/test_validate_holiday_date.py
@@ -11,41 +11,50 @@ class TestValidateHolidayDate(TestHrHolidaysBase):
         super().setUpClass()
 
     def test_account_analytic_line_date(self):
-        employee = self.employee_emp
         leave_type = self.env["hr.leave.type"].create(
             {
                 "name": "Paid Time Off",
-                "request_unit": "hour",
-                "double_validation": True,
-                "validity_start": fields.Datetime.from_string('2023-01-01 00:00:00'),
-                "validity_stop": fields.Datetime.from_string('2023-12-31 00:00:00')
+                "allocation_type": "fixed",
+                "validation_type": "hr",
+                "validity_start": False,
             }
         )
-        allocation = self.env["hr.leave.allocation"].create(
-            {
-                "name": "30 days allocation",
-                "holiday_status_id": leave_type.id,
-                "number_of_days": 30,
-                # "employee_id": employee.id,
-            }
+        allocation = (
+            self.env["hr.leave.allocation"]
+            .sudo(self.user_hruser_id)
+            .create(
+                {
+                    "name": "30 days allocation",
+                    "holiday_status_id": leave_type.id,
+                    "number_of_days": 30,
+                    "employee_id": self.employee_emp_id,
+                }
+            )
         )
         allocation.action_approve()
 
         # leave 1 only for 1 day or half
-        leave1 = self.env["hr.leave"].create(
-            {
-                "name": "Holiday 1 Day",
-                "employee_id": employee.id,
-                "holiday_status_id": leave_type.id,
-                "date_from": fields.Datetime.from_string("2023-12-11 08:00:00"),
-                "date_to": fields.Datetime.from_string("2023-12-11 17:00:00"),
-                "number_of_days": 1,
-            }
+        leave1 = (
+            self.env["hr.leave"]
+            .sudo(self.user_employee_id)
+            .create(
+                {
+                    "name": "Holiday 1 Day",
+                    "employee_id": self.employee_emp_id,
+                    "holiday_status_id": leave_type.id,
+                    "date_from": fields.Datetime.from_string("2023-12-11 08:00:00"),
+                    "date_to": fields.Datetime.from_string("2023-12-11 17:00:00"),
+                    "number_of_days": 1,
+                }
+            )
         )
-        leave1.action_approve()
-        leave1.action_validate()
+        leave1.sudo().action_approve()
+
         self.assertEqual(
-            leave1.timesheet_ids[0].date, leave1.date_from.date(), msg=None)
+            leave1.sudo().timesheet_ids[0].date,
+            leave1.sudo().date_from.date(),
+            msg=None,
+        )
 
         # leave 2 more than 1 day
         date_start = fields.Datetime.from_string("2019-12-27 08:00:00")
@@ -53,17 +62,15 @@ class TestValidateHolidayDate(TestHrHolidaysBase):
         leave2 = self.env["hr.leave"].create(
             {
                 "name": "Holiday 3 Days",
-                "employee_id": employee.id,
+                "employee_id": self.employee_emp_id,
                 "holiday_status_id": leave_type.id,
-                "date_from": date_start,
-                "date_to": date_end,
+                "request_date_from": date_start,
+                "request_date_to": date_end,
                 "number_of_days": 2,
             }
         )
-        leave2.action_approve()
-        leave2.action_validate()
+        leave2.sudo().action_approve()
+        # leave2.sudo(self.user_hrmanager_id).action_validate()
 
-        for timesheet_line in leave2.timesheet_ids:
-            self.assertIn(
-                timesheet_line.date,
-                [date_start.date(), date_end.date()])
+        for timesheet_line in leave2.sudo().timesheet_ids:
+            self.assertIn(timesheet_line.date, [date_start.date(), date_end.date()])
