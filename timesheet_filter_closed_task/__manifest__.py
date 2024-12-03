0024f4ac0240c145f4310f7e2e84a7078af601cc
Rivo Lalaina <rivo2302@gmail.com>
2024-12-03 11:59:43 +0300

    [ADD] [12.0] timesheet_filter_closed_task

--- timesheet_filter_closed_task/__manifest__.py
+++ timesheet_filter_closed_task/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://bit.ly/numigi-com",
     "license": "LGPL-3",
     "category": "Timesheets",
-    "summary": "Default task stages on projects.",
+    "summary": "Ensures that only open tasks when recording timesheets.",
     "depends": [
         "hr_timesheet",
         "hr_timesheet_sheet",
