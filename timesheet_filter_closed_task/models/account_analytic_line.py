e955da63b9a6295423e5470aaeec70f6c91dd6e9
Rivo Lalaina <rivo2302@gmail.com>
2024-12-04 06:05:37 +0300

    [ADD][12.0]  timesheet_filter_closed_task

--- timesheet_filter_closed_task/models/account_analytic_line.py
+++ timesheet_filter_closed_task/models/account_analytic_line.py
@@ -1,20 +1,17 @@
 # Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, models
 
 
 class AccountAnalyticLine(models.Model):
 
     _inherit = 'account.analytic.line'
 
-    task_id = fields.Many2one(
-        domain=lambda self: self._get_task_domain(),
-    )
-    
+    @api.model
     def _get_task_domain(self):
-        return [
-            ('stage_id.closed', '=', False),
-            ('project_id', '=', 'project_id'),
-            ('stage_id.allow_timesheet', '=', True),
-        ]
+        return "[" \
+               "('project_id', '=', project_id)," \
+               "('stage_id.allow_timesheet', '=', True)," \
+                "('stage_id.closed', '=', False)," \
+               "]"
