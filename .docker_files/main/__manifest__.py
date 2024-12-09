e579ff87a238226df4a13123c8b517dbb835769e
Rivo Lalaina <rivo2302@gmail.com>
2024-12-09 21:39:19 +0300

    [IMP] pre-commit : black hook (#50)

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -2,16 +2,16 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Main Module',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Install all addons required for testing.',
-    'depends': [
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": [
         "hr_timesheet",
     ],
-    'installable': True,
+    "installable": True,
 }
