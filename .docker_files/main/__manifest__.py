dae169375ec92075caec438d1648bd1a6b933ebd
Rivo Lalaina <rivo2302@gmail.com>
2025-01-09 11:17:47 +0300

    [16.0][ADD] timesheet_task_project_no_change

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -13,6 +13,7 @@
     "depends": [
         "hr_timesheet_project_parent",
         "project_timesheet_time_control_sheet",
+        "timesheet_task_project_no_change",
     ],
     "installable": True,
 }
