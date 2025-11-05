0519ad76b9e0570c6b6692df17724c6ff64eab31
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-11-05 16:16:38 +0100

    TA#80649 [IMP] timeshet_sheet_confirm_reset : fix fr.po module name (#64)
    * [IMP] timeshet_sheet_confirm_reset : fix fr.po module name
    
    * [IMP] timeshet_sheet_confirm_reset : fix fr.po module name
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

---
+++ hr_timesheet_groupby_task_origin/models/hr_timesheet_sheet.py
@@ -0,0 +1,42 @@
+# Copyright Numigi 2025 (tm) and all its contributors
+# (https://numigi.com/r/home)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class HrTimesheetSheet(models.Model):
+    _inherit = 'hr_timesheet.sheet'
+
+    def action_confirm_reset(self):
+        """
+        Allow the owner (employee's user) to reset a submitted sheet back
+        to draft, provided it is not approved. Raises errors if the sheet
+        is already approved or modified concurrently.
+        """
+        self.ensure_one()
+        current_uid = self.env.uid
+        if (
+            self.user_id.id != current_uid
+            and not self.env.user.has_group(
+                'hr_timesheet.group_hr_timesheet_user'
+            )
+        ):
+            raise UserError(_('You can only reset your own timesheets.'))
+
+        timesheet_id = self.with_context(
+            prefetch_fields=False).sudo().browse(self.id)
+        if not timesheet_id:
+            raise UserError(_('Timesheet not found.'))
+
+        if timesheet_id.state == 'done':
+            raise UserError(_(
+                'Error: You cannot reset a timesheet that has already '
+                'been approved.'
+            ))
+
+        if timesheet_id.state in ('draft', False):
+            raise UserError(_('The timesheet is already in draft state.'))
+
+        timesheet_id.sudo().write({'state': 'draft'})
---
+++ hr_timesheet_groupby_task_origin/tests/test_hr_timesheet_sheet.py
@@ -0,0 +1,80 @@
+# © Numigi 2025 (tm) and all its contributors
+# (https://numigi.com/r/home)
+# License LGPL-3.0 or later
+# (http://www.gnu.org/licenses/lgpl).
+
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
+
+
+class TestHrTimesheetSheetConfirmReset(TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+
+        # Create employee user
+        self.employee_user = self.env['res.users'].create({
+            'name': 'Test Employee',
+            'login': 'employee_test',
+        })
+
+        # Assign hr_timesheet_user group
+        group = self.env.ref('hr_timesheet.group_hr_timesheet_user')
+        group.users = [(4, self.employee_user.id)]
+
+        # Create employee linked to user
+        self.employee = self.env['hr.employee'].create({
+            'name': 'Test Employee',
+            'user_id': self.employee_user.id,
+        })
+
+        # Create reviewer user
+        self.reviewer_user = self.env['res.users'].create({
+            'name': 'Reviewer',
+            'login': 'reviewer_test',
+        })
+
+        # Assign timesheet approver group
+        reviewer_group = self.env.ref(
+            'hr_timesheet.group_hr_timesheet_approver'
+        )
+        reviewer_group.users = [(4, self.reviewer_user.id)]
+
+        # Create reviewer employee
+        self.reviewer = self.env['hr.employee'].create({
+            'name': 'Reviewer',
+            'user_id': self.reviewer_user.id,
+        })
+
+        # Create timesheet in 'confirm' state with reviewer
+        self.timesheet = self.env['hr_timesheet.sheet'].sudo().create({
+            'name': 'Test Timesheet',
+            'employee_id': self.employee.id,
+            'reviewer_id': self.reviewer.id,
+            'state': 'draft',
+        })
+
+    def test_reset_to_draft_success(self):
+        """Employee resets their own submitted sheet to draft."""
+        self.timesheet = self.timesheet.with_user(self.employee_user)
+        # Submit the sheet
+        self.timesheet.action_timesheet_confirm()
+        # Reset to draft
+        self.timesheet.action_confirm_reset()
+        self.assertEqual(self.timesheet.state, 'draft')
+
+    def test_reset_to_draft_approved_error(self):
+        """Resetting an approved timesheet raises an error."""
+        # Approve the sheet
+        self.timesheet.reviewer_id = self.reviewer.id
+        self.timesheet = self.timesheet.with_user(self.reviewer_user)
+        self.timesheet.action_timesheet_confirm()
+        self.timesheet.action_timesheet_done()
+        # Employee tries to reset approved sheet
+        self.timesheet = self.timesheet.with_user(self.employee_user)
+        with self.assertRaises(UserError) as e:
+            self.timesheet.action_confirm_reset()
+        self.assertIn(
+            'You cannot reset a timesheet that has already been approved',
+            str(e.exception)
+        )
