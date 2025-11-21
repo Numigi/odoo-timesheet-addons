5c08115c8a669e72bde52746d373331806a5445b
Abdellatif Benzbiria <odoo-base@numigi.com>
2025-11-21 19:53:44 +0100

    [ADD] timesheet_project_task_validation

--- timesheet_project_task_validation/__manifest__.py
+++ timesheet_project_task_validation/__manifest__.py
@@ -1,22 +1,24 @@
-# Copyright 2025 Numigi (tm) and all its contributors (https://numigi.com/r/home)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2025 Numigi (tm)
+# and all its contributors (https://numigi.com/r/home)
+# License AGPL-3.0 or later
+# (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Timesheet Projetc Task Validation",
+    "name": "Timesheet Project Task Validation",
     "version": "14.0.1.0.0",
     "author": "Numigi",
-    "summary": "Ensures consistency between project and task in timesheets.",
+    "summary": (
+        "Ensures consistency between project and task "
+        "in timesheets."
+    ),
     "maintainer": "Numigi",
     "website": "https://numigi.com/r/home",
     "license": "AGPL-3",
     "category": "Project",
     "depends": [
-            "hr_timesheet_sheet",
-            "project"
-                ],
-    "data": [
-        #"views/hr_timesheet_sheet_view.xml",
-        #"views/account_analytic_line_view.xml",
+        "hr_timesheet_sheet",
+        "project",
     ],
+    "data": [],
     "installable": True,
 }
